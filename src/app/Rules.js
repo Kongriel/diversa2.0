5c2461a5f06606340b5801d28ca08718ba08e269
Anton <krauseriel@gmail.com>
2024-04-24 12:58:11 +0200

    ye

--- src/app/Rules.js
+++ src/app/Rules.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const Rules = () => {
   return (
     <>
-    <h1>Rule overview</h1>
-    <ol>
-        <li> <Link to="/rules/1">Regl 1</Link></li>
-        <li> <Link to="/rules/2">Regl 2</Link></li>
-        <li> <Link to="/rules/3">Regl 3</Link></li>
-        <li> <Link to="/rules/4">Regl 4</Link></li>
-    </ol>
+      <h1>Rule overview</h1>
+      <ol>
+        <li>
+          {" "}
+          <Link to="/rules/1">Regl 1</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/rules/2">Regl 2</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/rules/3">Regl 3</Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="/rules/4">Regl 4</Link>
+        </li>
+      </ol>
     </>
-  )
-}
+  );
+};
 
 export default Rules;
-
-//Forklaring
-// Hvert link peger til en dynamisk route hvor ruleID er specificeret i URL'en
-// Når et link klikkes navigeres brugeren til den relaterede description page
-// Denne side fungerer som en main page hvor brugeren kan se en liste over de forskellige regler
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,10 @@ import Image from "next/image";
 
 export default async function Page() {
   return (
-    <>
-      <div>
-        <h1> Test din Side</h1>
-
-        <div></div>
-        <p>blablbalbla</p>
+    <main className="bg-brand-beige-10 flex flex-col items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-brand-orange-70"> Test din Side</h1>
+        <p className="text-brand-orange-70">blablbalbla</p>
       </div>
 
       <form action="/result" className="mt-4">
@@ -17,11 +15,12 @@ export default async function Page() {
         <label htmlFor="url" className="text-orange-700">
           Indtast URL:
         </label>
-        <input type="url" name="url" id="url" className="border border-gray-300 rounded-md px-3 py-2 w-full text-gray-700 focus:outline-none focus:border-orange-500" />
+        <input type="url" name="url" id="url" placeholder="www.yourwebsite.com" className="border border-gray-300 rounded-md px-3 py-2 w-64 text-brand-orange-70 placeholder-orange-70 focus:outline-none focus:border-orange-500" />
+        {/* Adjusted width to w-64 */}
         <button type="submit" className="mt-2 bg-orange-700 text-white py-2 px-4 rounded-md hover:bg-orange-600">
           Resultat
         </button>
       </form>
-    </>
+    </main>
   );
 }
