bb49a3f88c3e715cf9a3f2b192f7a2ca95d4e043
Anton <krauseriel@gmail.com>
2024-04-25 09:55:05 +0200

    jj

--- src/app/result/error.jsx
+++ src/app/result/error.jsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 
 export default function Error() {
   return (
-    <div className="bg-brand-beige-10 flex flex-col items-center justify-center h-screen" id="oopss">
-      <div id="error-text" className="text-brand-orange-70 text-center">
+    <div className="bg-brand-beige-10 flex flex-col items-center justify-center h-screen">
+      <div className="text-brand-orange-70 text-center">
         <Image src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404" width={200} height={200} />
         <h1 className="text-4xl font-bold my-4">404 PAGE</h1>
         <p className="text-lg mb-2">The page you were looking for could not be found</p>
