b32f5c5976c802365e907d5f2ec9882796086d5d
Anton <krauseriel@gmail.com>
2024-04-25 10:06:10 +0200

    yeboi

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,17 +28,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-libre-baskerville)"],
-        mono: ["var(--font-poppins)"],
-      },
-    },
-  },
-  plugins: [],
-};
