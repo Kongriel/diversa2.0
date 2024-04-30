7663e9c6e4eeda6666ac3782661514c69c2aa698
August <augustjp@live.dk>
2024-04-30 17:02:22 +0200

    Updated rules

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
+import rulesData from "./json/rules.json";
 
 export const revalidate = 1200;
 
@@ -9,6 +9,12 @@ export default async function ResultPage({ searchParams }) {
   const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
   const data = await response.json();
   console.log(response);
+
+  const rulesList = Object.entries(rulesData).map(([slug, rule]) => ({
+    slug,
+    name: rule.headline
+  }));
+
   return (
     <main>
       <div>
@@ -16,42 +22,12 @@ export default async function ResultPage({ searchParams }) {
           <h1>REGLER</h1>
           <h2>Oversigt over de regler der testes for</h2>
           <ol className="border border-gray-300 p-4">
-            <li className="text-blue-500 border border-gray-300 p-2">
-              <Link href="rules/landmark-no-duplicate-banner">landmark-no-duplicate-banner</Link>
-            </li>
-            <li>
-              <Link href="rules/landmark-complementary-is-top-level">landmark-complementary-is-top-level</Link>
-            </li>
-            <li>
-              <Link href="rules/label-title-only">label-title-only</Link>
-            </li>
-            <li>
-              <Link href="rules/aria-dialog-name">aria-dialog-name</Link>
-            </li>
-            <li>
-              <Link href="rules/landmark-unique">landmark-unique</Link>
-            </li>
-            <li>
-              <Link href="rules/image-redundant-alt">image-redundant-alt</Link>
-            </li>
-            <li>
-              <Link href="rules/page-has-heading-one">page-has-heading-one</Link>
-            </li>
-            <li>
-              <Link href="rules/heading-order">heading-order</Link>
-            </li>
-            <li>
-              <Link href="rules/empty-heading">empty-heading</Link>
-            </li>
-            <li>
-              <Link href="rules/region">region</Link>
-            </li>
-            <li>
-              <Link href="rules/tabindex">tabindex</Link>
-            </li>
-            <li>
-              <Link href="rules/landmark-one-main">landmark-one-main</Link>
-            </li>
+
+            {rulesList.map((rule, index) => (
+              <li key={index} className="text-blue-500 border border-gray-300 p-2">
+                <Link href={`rules/${rule.slug}`}>{rule.name}</Link>
+              </li>
+            ))}
           </ol>
 
           <button>
