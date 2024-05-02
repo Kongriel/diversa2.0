eb52c26d8cf3430df38a73bf9e37cd8268a536f3
August <augustjp@live.dk>
2024-05-02 15:07:14 +0200

    footer updated

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
   return (
-    <div>
-      <p>Hej</p>
-    </div>
+    <footer className="bg-gray-200 py-4 text-center">
+      <p className="text-gray-600">&#169; Diversa 2024</p>
+    </footer>
   );
 }
 
