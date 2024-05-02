37e92c8dac5352d0387187426a91195668840495
August <augustjp@live.dk>
2024-05-02 15:17:18 +0200

    current year is now dynamic

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-gray-200 py-4 text-center">
-      <p className="text-gray-600">&#169; Diversa 2024</p>
+      <p className="text-gray-600">&#169; Diversa {currentYear}</p>
     </footer>
   );
 }
