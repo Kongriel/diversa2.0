43f6d052c6a4f58e99ebb084fc00a1d387d03974
August <augustjp@live.dk>
2024-05-02 14:51:57 +0200

    responsive li-elementer på rulelist

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -28,13 +28,14 @@ export default async function ResultPage({ searchParams }) {
           <h2 className="justify-center mx-auto mb-5 text-xl font-normal leading-7 text-center  font-poppins">
             Overview of the rules being tested
           </h2>
-          <ol className="p-4">
+          <ol className="p-4 flex flex-col items-center">
             {rulesList.map((rule, index) => (
               <li
                 key={index}
-                className="p-8 bg-white shadow-glass-1 hover:shadow-lg rounded-2xl mb-5 flex justify-between items-center	"
+                className="p-8 bg-white shadow-glass-1 hover:shadow-lg rounded-2xl mb-5 flex justify-between items-center sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl"
+                style={{ width: "100%", maxWidth: "40rem" }} // Adjust the max-width as needed
               >
-                <p className="">{rule.name}</p>
+                <p className="flex-grow">{rule.name}</p>
                 <Link
                   href={`rules/${rule.slug}`}
                   className="mt-4 sm:mt-0 sm:ml-6 px-5 py-3 text-base font-medium text-white transition-colors bg-blue-500 border border-transparent rounded-md shadow disabled:bg-blue-400 sm:w-auto sm:px-10 w-fit whitespace-nowrap"
@@ -45,11 +46,11 @@ export default async function ResultPage({ searchParams }) {
             ))}
           </ol>
 
-  <div className="flex justify-center m-4">
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow justify-center">
-          <Link href="/">Home</Link>
-</button>
-</div>
+          <div className="flex justify-center m-4">
+            <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow justify-center">
+              <Link href="/">Home</Link>
+            </button>
+          </div>
         </div>
       </div>
     </main>
