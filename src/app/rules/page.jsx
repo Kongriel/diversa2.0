5344b3cc29573bc51431552803e66a41866cdd72
Anton <krauseriel@gmail.com>
2024-05-02 15:20:57 +0200

    yeboiii

--- src/app/rules/page.jsx
+++ src/app/rules/page.jsx
@@ -6,26 +6,30 @@ export const revalidate = 1200;
 
 export default async function ResultPage({ searchParams }) {
   const params = new URLSearchParams(searchParams);
-  const response = await fetch(`https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}`);
+  const response = await fetch("https://mmd-a11y-api.vercel.app/api/scan?${params.toString()}");
   const data = await response.json();
   console.log(response);
 
   //JSON to array
   const rulesList = Object.entries(rulesData).map(([slug, rule]) => ({
     slug,
-    name: rule.headline,
+    name: rule.title,
   }));
 
   return (
-    <main className="">
+    <main>
       <div>
         <div>
           <h1 className="mt-5 mb-6 text-6xl font-extrabold leading-none tracking-tight text-center">Rules</h1>
           <h2 className="justify-center mx-auto mb-5 text-xl font-normal leading-7 text-center  font-poppins">Overview of the rules being tested</h2>
-          <ol className="p-4">
+          <ol className="p-4 flex flex-col items-center">
             {rulesList.map((rule, index) => (
-              <li key={index} className="p-8 bg-white shadow-glass-1 hover:shadow-lg rounded-2xl mb-5 flex justify-between items-center	">
-                <p className="">{rule.name}</p>
+              <li
+                key={index}
+                className="p-8 bg-white shadow-glass-1 hover:shadow-lg rounded-2xl mb-5 flex justify-between items-center sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl"
+                style={{ width: "100%", maxWidth: "40rem" }} // Adjust the max-width as needed
+              >
+                <p className="flex-grow text-2xl font-poppins ">{rule.name}</p>
                 <Link href={`rules/${rule.slug}`} className="mt-4 sm:mt-0 sm:ml-6 px-5 py-3 text-base font-medium text-white transition-colors bg-blue-500 border border-transparent rounded-md shadow disabled:bg-blue-400 sm:w-auto sm:px-10 w-fit whitespace-nowrap">
                   Read more
                 </Link>
